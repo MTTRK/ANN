6a889ab01bd6e7e6f8f5a59670edd04f72b4b0b1
Mate Torok <mate.torok.de@gmail.com>
2017-02-25 23:26:04 +0100

    Structural change only

--- WarmUp/main/rnn_lstm.py
+++ WarmUp/main/rnn_lstm.py
@@ -10,36 +10,56 @@ from keras.layers import Dense
 from keras.layers import LSTM
 
 
-random_seed = 561
-alphabet = 26
-cell_num = 13
-epochs = 1000
-batch_size = 26
+def create_model(seed):
+    np.random.seed(seed)
+    return Sequential()
 
-np.random.seed(random_seed)
-dataX = np.identity(alphabet)
-X = np.reshape(dataX, (len(dataX), alphabet, 1))
-Y = np.asarray([1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
 
-model = Sequential()
-model.add(
-    LSTM(
-        input_shape=[alphabet, 1],
-        output_dim=cell_num))
-model.add(
-    Dense(
-        output_dim=1,
-        activation='sigmoid'))
+def train_model(model, X, Y, epochs, batches, lossf, opt):
+    model.compile(loss=lossf, optimizer=opt, metrics=['accuracy'])
+    print(model.summary())
+    model.fit(X, Y, nb_epoch=epochs, batch_size=batches)
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-print(model.summary())
+def add_lstm_layer(model, in_shape, cell_num):
+    return model.add(
+        LSTM(input_shape=in_shape,
+            output_dim=cell_num))
 
-model.fit(X, Y, nb_epoch=epochs, batch_size=batch_size)
 
-scores = model.evaluate(X, Y, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+def add_dense_layer(model, in_dim, out_dim, _activation):
+    return model.add(
+        Dense(input_dim=in_dim,
+            output_dim=out_dim,
+            activation=_activation)
+    )
 
-predictions = model.predict(X)
-rounded = [x.round()[0] for x in predictions]
-print(rounded)
+
+def predict(model, X):
+    return model.predict(X)
+
+
+def run():
+    seed = 561
+    alphabet = 26
+    cell_num = 13
+    epochs = 1000
+    batches = 26
+
+    dataX = np.identity(alphabet)
+    X = np.reshape(dataX, (len(dataX), alphabet, 1))
+    Y = np.asarray([1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+
+    model = create_model(seed)
+    add_lstm_layer(model, [alphabet, 1], cell_num)
+    add_dense_layer(model, cell_num, 1, 'sigmoid')
+    train_model(model, X, Y, epochs, batches, 'binary_crossentropy', 'adam')
+
+    scores = model.evaluate(X, Y, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1]*100))
+
+    predictions = predict(model, X)
+    print([x.round()[0] for x in predictions])
+
+
+run()
